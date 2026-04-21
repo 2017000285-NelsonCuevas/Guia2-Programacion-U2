ebdaca51783941553791cb9e30bf3722c6da9059
2017000285-NelsonCuevas <n.cuevas.5adb@gmail.com>
2026-04-20 22:13:01 -0600

    Add files via upload

---
+++ Ejercicio8.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("===MENUUU===");
+        Console.WriteLine("1. = HAMBURGUESA");
+        Console.WriteLine("2. = PIZZA");
+        Console.WriteLine("3. = POLLO FRITO");
+        Console.WriteLine("4. = ENSALADA");
+        int menu;
+        Console.WriteLine("Ingresa un numero para el menu del 1 al 4: ");
+        menu = Convert.ToInt32(Console.ReadLine());
+
+        switch (menu)
+        {
+            case 1:
+                Console.WriteLine("HAMBURGUESA");
+                break;
+            case 2:
+                Console.WriteLine("PIZZA");
+                break;
+            case 3:
+                Console.WriteLine("POLLO FRITO");
+                break;
+            case 4:
+                Console.WriteLine("ENSALDA");
+                break;
+            default:
+                Console.WriteLine("INGRESA UN NUMERO ADECUADO PARA ORDENAR");
+                break;
+        }
+    }
+}
